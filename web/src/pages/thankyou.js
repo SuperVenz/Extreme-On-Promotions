8f832e0ff3c92eef0131e88edeccc26861b0bbf2
Alec Venzor <alecvenzordev@gmail.com>
2021-12-06 22:58:28 -0800

    .

--- web/src/pages/thankyou.js
+++ web/src/pages/thankyou.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
+import styled from "styled-components";
+import BackBar from "../components/BackBar";
+const Wrapper = styled.div`
+  text-align: center;
+  padding: 24px;
+`;
 function submit({ data }) {
   return (
     <Layout
@@ -8,8 +14,11 @@ function submit({ data }) {
       keywords={data.sanityThankYou.seo.keywords}
       description={data.sanityThankYou.seo.description}
     >
-      <h1>Thank You!</h1>
-      <p>Form Submitted</p>
+      <BackBar />
+      <Wrapper>
+        <h1>Thank You!</h1>
+        <p>Form Submitted</p>
+      </Wrapper>
     </Layout>
   );
 }
